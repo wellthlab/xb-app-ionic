fe332ee5306ac52ebeb619965dcc8d97c4d78a9d
Richard Gomer <r.gomer@soton.ac.uk>
2020-11-03 17:42:48 +0000

    Rearrange some assets to look more like the app we want!

--- xbapp/src/App.tsx
+++ xbapp/src/App.tsx
@@ -10,10 +10,9 @@ import {
   IonTabs
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import { cubeOutline, personCircle } from 'ionicons/icons';
+import TabExp from './pages/TabExp';
+import TabAccount from './pages/TabAccount';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -39,23 +38,18 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route path="/tab1" component={Tab1} exact={true} />
-          <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab3" component={Tab3} />
-          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+          <Route path="/exp" component={TabExp} exact={true} />
+          <Route path="/account" component={TabAccount} exact={true} />
+          <Route path="/" render={() => <Redirect to="/exp" />} exact={true} />
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+          <IonTabButton tab="exp" href="/exp">
+            <IonIcon icon={cubeOutline} />
+            <IonLabel>Experiments</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+          <IonTabButton tab="account" href="/account">
+            <IonIcon icon={personCircle} />
+            <IonLabel>Account</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
---
+++ xbapp/src/pages/TabAccount.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './TabAccount.css';
+
+const TabAccount: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Profile</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tab 2</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default TabAccount;
