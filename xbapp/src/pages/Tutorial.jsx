7daf41f4309802ff01c481610daf5086d9bf4340
George Muresan <gcm1u16@soton.ac.uk>
2020-12-03 21:59:46 +0000

    tutorial updated

--- xbapp/src/pages/Tutorial.jsx
+++ xbapp/src/pages/Tutorial.jsx
@@ -192,13 +192,27 @@ class Tutorial extends Component {
               </IonItem>
               <IonCardHeader>
                 <IonCardTitle>Experiments in a Box.</IonCardTitle>
-                <IonCardSubtitle>What is our approach?</IonCardSubtitle>
+                <IonCardSubtitle>What is it?</IonCardSubtitle>
               </IonCardHeader>
               <IonCardContent>
-                You already have a lot of practice Eating. Sleeping. Moving. And that's great! But the question is, is there more to it? Is there something which works for YOU and doesn't work for others?<br></br>
-                A practice? A skill? A piece of knowledge? A habit? Our answer is: An experiment! Experiments-In-A-Box is our approach to make YOU Move better! Eat better! Engage Better! Cogitate Better! And Sleep Better! Our plan is to make you FEEL better!<br></br>
-                How? By offering you a variety of "BOXES" - sets of different experiments which can work for you and your daily habits, and we give you the opportunity of experimenting with them to see.. which one brings you more skills, which one improves your lifestyle.. and which one improves your WELLBEING!<br></br>
-                And the best thing.. You can even create your own! </IonCardContent>
+              Welcome to an Experiment in a Box -  Here to Tune in what can help you feel better.<br></br>
+              The idea of an experiment in a box - or XB for short - is to help you build helpful health practices by letting you test them out - to see what works for you.<br></br>
+              WHAT IS AN EXPERIMENT? An experiment is a way to test an idea: does something you think will have a particular effect in a particular condition have that effect? For example: if you are a coffee drinker, you may wish to test the concept that not drinking coffee after 1pm means feeling more refreshed the next morning. For a week, you may commit to making sure you don’t drink any coffee after 1pm. You may pay attention to how you feel over the course of the week, and notice that by about day 4 you are feeling more refreshed in the morning.  <br></br>
+              You now have some new, tested information about how coffee may be affecting your sleep quality. 
+<br></br>
+These are exactly the kinds of experiments an XB supports. The XB provides you with tools to explore health practices, to figure out how those practices can work for you: some may work better than others; some may be easier to implement than others. The XB gives you the space to find out. 
+<br></br>
+<b>Experiments are Short and Specific:</b> An XB only lasts a short time - most are 5-7 days - just enough for your body to get a sense of whether or not the approach is having a noticeableeffect. An experiment doesn’t ask you to commit to something forever. It says: try it; check it out, and if it works, keep it!
+<br></br>
+<b>Experiments are Not Life:</b> Some experiments may seem a bit odd - we have one to help folks explore how vegetables affect energy and mood where we suggest have a green vegetable each time you eat for a week. Now after the experiment, you may never eat that way again - but that’s ok: an experiment is not “normal” - it’s sometimes a little weird to be able to focus on the particular effect of the new thing you’re trying. But after the experiment, you have new knowledge that you can draw on when shaping your own eating practices. <br>
+</br>
+One of the comments we’ve had from folks carrying out the Experiments in a Box is that it gives them permission to take the time to explore something. <br>
+</br>
+As one Experimenter said “i’ve heard that 8 hours of sleep a night is good for you, but if i hadn’t tried that as an experiment in a box, i would never have really found out. IT’s changed my life” - 
+<br></br>    
+Not all experiment experiences may be that profound, but we hope they do help you give yourself permission to try something in this focused way, for just enough time to find out - how does it feel; do you feel better? <br></br>
+                
+                </IonCardContent>
             </IonCard>
           </IonSlide>
           <IonSlide>
@@ -215,15 +229,13 @@ class Tutorial extends Component {
                 <IonCardSubtitle>How does it work?</IonCardSubtitle>
               </IonCardHeader>
               <IonCardContent>
-                The good aspect to our approach is that we do not tell you what to do.<br></br>
-                We simply give you a few health habits you can try out to see if they work FOR YOU?<br></br>
-                Why? Because EVERYONE IS DIFFERENT. So why not just try out some wellbeing practices to see which one is most beneficial for you?<br></br>
-                WE give you the options of experimentation.<br></br>
-                YOU get the power to choose what experiment you are doing.<br></br>
-                YOU feed YOUR DATA back into the app.<br></br>
-                YOU reflect on the experiment's effect on your wellbeing.<br></br>
-                YOU decide whether you wish to incorporate this into your daily life.<br></br>
-                And that's it! Simple as that! We give you the options, and you control your experience!
+              An Experiment in a Box has several features designed to make it as useful for you as possible. It starts with the idea that YOU are the scientist running this experiment, and the box gives you the tools to carry out your study. The box has several features, listed below:<br>
+              </br>
+              <b>1. You Choose:</b> An experiment in a box may have many ways to explore a health practice. For instance, for sleep, it might help you explore light and dark levels for a week, or noise, or killing your alarm or many other practices tested to support better sleep. It’s up to you: pick from the box the approach that feels interesting to you. <br>
+              </br>
+              <b>2. Guides to Help you record your results:</b> A key part of a scientist’s work, of course, is to create the evidence about whether or not the thing being tested is having the effect anticipated or not. The experiment in a box app provides tools to capture key bits of data to build up your evidence for analysis. A key question the XB will always ask each day is: how do you feel: the same, better, much better; worse, much worse. All data is good data. Other features may include daily short questionnaires to help you reflect on the effect of what you’re currently testing.<br></br>
+              <b>3. Visualise your Evidence:</b> Once you have gathered some results, there are tools to help you see what the data may be telling you. For instance, in an XB exploring movement and time of day, you may see in a graph that shows how you feel with movement times, that you seem to feel better on days after you move in the morning rather than the afternoon. <br></br>
+              <b>4.Working Together:</b> Sometimes it helps to work with others on a Study to share experiences, build insights and develop understanding. XBs support creating Teams where you can each carry out the same study, and share your findings. For instance, in a Move study, you may find that you are not alone: many other people like to go for runs in the rain, too, and that morning rain runs seem to feel better for more people. <br></br>
                 </IonCardContent>
             </IonCard>
           </IonSlide>
@@ -237,64 +249,18 @@ class Tutorial extends Component {
                 </IonGrid>
               </IonItem>
               <IonCardHeader>
-                <IonCardTitle>XB: Visualise.</IonCardTitle>
-                <IonCardSubtitle>What do we mean by "feed your data"?</IonCardSubtitle>
+                <IonCardTitle>An XB Team.</IonCardTitle>
+                <IonCardSubtitle>What is a team?</IonCardSubtitle>
               </IonCardHeader>
               <IonCardContent>
-                You have total control over your data. We are just giving you some helpers to help MANAGE your data, ANALYSE it, and VISUALISE it.<br></br>
-                Amongst the features in the app of editing your profile, choosing an experiment and trying out a new health practice, you will be able to (1) record your experience and (2) reflect on your experience.<br></br>
-                We are doing that by prompting you with questions about your experiment, your experiment, your daily mood and health, which is later going to help you understand how these health practices impact you!<br></br>
-                And that's not everything! These are not only adjustable, but also optional! YOU decide what data you want to feed back to the app.<br></br>
-                And while browsing all of our features, you may find the "Goal Diary" (personal notebook), the "Calendar" (record of all your data), and the "Visualise" (graphs to understand your progress) sections be of great help in your journey!
+              When you start any XB you’ll get a number - that’s your own XB code. 
+If you’d like to invite someone else to run the same experiment with you, you can share your code with them. When they start up the XB app, they can choose “join a team.” 
+<br></br>
+When they hit that button, they can enter the code you’ve given them, and they’ll be automatically added to the team/the experiment - and the team will show up on the list of experiments they are part of!<br></br>
                 </IonCardContent>
             </IonCard>
           </IonSlide>
-          <IonSlide>
-          <IonCard>
-              <IonItem>
-                <IonGrid>
-                  <IonRow>
-                    <IonCol><img style={{width: "100px", resize: "both"}} src="assets/box.png" alt="XB Logo" /></IonCol>
-                  </IonRow>
-                </IonGrid>
-              </IonItem>
-              <IonCardHeader>
-                <IonCardTitle>An XB Group.</IonCardTitle>
-                <IonCardSubtitle>What's a group?</IonCardSubtitle>
-              </IonCardHeader>
-              <IonCardContent>
-                And there's more!<br></br>
-                We understand you may want to experience these health practices by yourself. You can look at YOUR own data.<br></br>
-                BUT: what is you want to (1) compare with your friends, or even (2) compare with other individuals?<br></br>
-                Well, XB Groups is a function which lets you join your XB experiment as part of a group, and work through the experiments together - OR - compare your data/your group's data with other groups/individuals!<br></br>
-                This shall be well fun!
-                </IonCardContent>
-            </IonCard>
-          </IonSlide>
-          <IonSlide>
-          <IonCard>
-              <IonItem>
-                <IonGrid>
-                  <IonRow>
-                    <IonCol><img style={{width: "100px", resize: "both"}} src="assets/box.png" alt="XB Logo" /></IonCol>
-                  </IonRow>
-                </IonGrid>
-              </IonItem>
-              <IonCardHeader>
-                <IonCardTitle>XB.</IonCardTitle>
-                <IonCardSubtitle>Join today!</IonCardSubtitle>
-              </IonCardHeader>
-              <IonCardContent>
-                1) CHOOSE.<br></br>
-                2) EXPERIMENT.<br></br>
-                3) RECORD.<br></br>
-                4) VISUALISE.<br></br>
-                5) REFLECT.<br></br>
-                6) IMPROVE.<br></br>
-                </IonCardContent>
-                <IonButton routerLink="/login" expand="full">Back to the Log In page -&gt;</IonButton>
-            </IonCard>
-          </IonSlide>
+
         </IonSlides>
       </IonContent>
     );
