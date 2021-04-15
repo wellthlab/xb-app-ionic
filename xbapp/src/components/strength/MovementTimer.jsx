153eb53b85f4fb4d4f1a370b385979c290dacd2d
Richard Gomer <richard@richardgomer.co.uk>
2021-04-15 18:47:41 +0100

    Fix MovementTimer

--- xbapp/src/components/strength/MovementTimer.jsx
+++ xbapp/src/components/strength/MovementTimer.jsx
@@ -14,24 +14,22 @@ import "./MovementTimer.scss";
  */
 const MovementTimer = ({ exercises, onSetChange, onDone, mins, day }) => {
   return (
-    <>
       <div id="movementTimer">
         <p style={{"padding": "5px 8px 5px 8px"}}>Repeat these two moves. Do ten of the first exercise, followed by ten of the second, for a total of <strong>{mins} minutes</strong>.</p>
         <p style={{"padding": "5px 8px 5px 8px"}}>Keep a running count of your sets, using the set counters. (A set is ten repetitions of a single exercise.)</p>
 
-      <CountDown minutes={mins} timerName={"day-" + day} onFinish={onDone} />
+        <CountDown minutes={mins} timerName={"day-" + day} onFinish={onDone} />
 
-      {exercises.map((ex, i) => {
-        var move = getMove(ex);
-        console.log("Get move", ex, move);
-        return <MovementInfoCard key={move.id} images={move.images} name={move.name}>
-          <SetCounter onChange={ (sets) => {
-            onSetChange(move.id, sets);
-          }} />
-        </MovementInfoCard>
-      })}
-
-    </>
+        {exercises.map((ex, i) => {
+          var move = getMove(ex);
+          console.log("Get move", ex, move);
+          return <MovementInfoCard key={move.id} images={move.images} name={move.name}>
+            <SetCounter onChange={ (sets) => {
+              onSetChange(move.id, sets);
+            }} />
+          </MovementInfoCard>
+        })}
+      </div>
   );
 };
 
