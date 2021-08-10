7d1ed806c3f041cc92aba9ef7f7acdc76f1fec4c
jdsmith104 <jdsmith104@outlook.com>
2021-08-10 10:21:37 +0100

    Updated star icon. Removed random number generated. Improved styling

--- xbapp/src/components/Tile.jsx
+++ xbapp/src/components/Tile.jsx
@@ -7,9 +7,11 @@ import {
   IonCardContent,
   IonCardSubtitle,
   IonText,
+  IonIcon,
 } from "@ionic/react";
 import "./Tile.scss";
 import SelectExerciseButton from "./SelectExerciseButton";
+import { star } from "ionicons/icons";
 
 function Tile(props) {
   const movement = props.movement || {};
@@ -27,7 +29,7 @@ function Tile(props) {
   // TODO: implement later
   function getTimesCompleted(movement) {
     const id = movement.id;
-    return Math.floor(Math.random() * 999);
+    return ""; // Math.floor(Math.random() * 999);
   }
 
   return (
@@ -47,7 +49,16 @@ function Tile(props) {
             <IonCardSubtitle>Level {progressionLevel}</IonCardSubtitle>
           </div>
           <div className="completed-container">
-            <p>* {completedCount}</p>
+            <IonIcon
+              icon={star}
+              style={{
+                marginBottom: "2px",
+                height: "19px",
+              }}
+            ></IonIcon>
+            <p style={{ height: "19px", marginLeft: "5px" }}>
+              {String(getTimesCompleted(movement))}
+            </p>
           </div>
         </IonCardHeader>
         <IonCardContent className="tile-content">
