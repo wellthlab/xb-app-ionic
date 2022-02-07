167488d3f9b60183bacd7639d85f27910cee39a9
Edward Parkinson <e.parkinson@soton.ac.uk>
2022-02-07 16:17:47 +0000

    prototype timer/move for no tasks

--- xbapp/src/Instruments/RecordMovement.jsx
+++ xbapp/src/Instruments/RecordMovement.jsx
@@ -1,16 +1,61 @@
 import React, { useState, useEffect } from "react";
-import { IonContent, IonPage, IonTitle } from "@ionic/react";
+import {
+  IonContent,
+  IonPage,
+  IonTitle,
+  IonDatetime,
+  IonHeader,
+} from "@ionic/react";
 import "./RecordMovement.scss";
 import { connect } from "react-redux";
 import XBHeader from "../util/XBHeader";
 
 function RecordMovement(props) {
+  const [activeDay, setActiveDay] = useState(new Date());
+  const [tasks, setTasks] = useState(null);
+
+  // let entry;
+  // let entries = group.entries;
+  // let dayList = [];
+  // for (var i in entries) {
+  //   if (entries[i].day === activeDay) {
+  //     entry = entries[i];
+  //   }
+  //   dayList.push(entries[i].day);
+  // }
+  //
+  // if (typeof entry === "undefined") {
+  //   return <>Can't find entry for day {activeDay}</>;
+  // }
+
+  const loadTasks = async function (dates) {};
+
+  useEffect(() => {
+    loadTasks(activeDay);
+  }, []);
+
+  let content;
+
+  if (!tasks) {
+    content = (
+      <>
+        <IonTitle>You have no tasks today.</IonTitle>
+      </>
+    );
+  } else {
+    content = "You have tasks today.";
+  }
+
+  const date = new Date().toString();
+
   return (
     <IonPage>
       <XBHeader title="Add Movement"></XBHeader>
       <IonContent fullscreen>
-        <br></br>
-        <IonTitle>Today you will</IonTitle>
+        <IonHeader>
+          <IonDatetime value={date} color="light" />
+        </IonHeader>
+        {content}
       </IonContent>
     </IonPage>
   );
