60940b8e60d1f3b1c7a245909ce4e2c5f8a8cf59
Edward Parkinson <e.parkinson@soton.ac.uk>
2022-02-16 13:53:33 +0000

    change some questions to selections

--- xbapp/src/Strength/Questions.jsx
+++ xbapp/src/Strength/Questions.jsx
@@ -13,10 +13,12 @@ import {
   IonCol,
   IonCardSubtitle,
   IonCardHeader,
+  IonSelect,
+  IonSelectOption,
 } from "@ionic/react";
 import { arrowForwardOutline } from "ionicons/icons";
 
-function Question({ label, type, setValue, readOnly }) {
+function QuestionText({ label, type, setValue, disabled }) {
   return (
     <>
       <IonCol>
@@ -27,10 +29,34 @@ function Question({ label, type, setValue, readOnly }) {
             onIonChange={(e) => {
               setValue(e.detail.value);
             }}
-            readonly={readOnly}
+            disabled={disabled}
           ></IonInput>
         </IonItem>
       </IonCol>
+    </>
+  );
+}
+
+function QuestionChoice({ label, choices, value, setValue, disabled }) {
+  let selections = choices.map((choice) => {
+    return <IonSelectOption value={choice}>{choice}</IonSelectOption>;
+  });
+
+  return (
+    <>
+      <IonItem>
+        <IonLabel position="floating">{label}</IonLabel>
+        <IonSelect
+          multiple={false}
+          value={value}
+          onIonChange={(e) => {
+            setValue(e.detail.value);
+          }}
+          disabled={disabled}
+        >
+          {selections}
+        </IonSelect>
+      </IonItem>
     </>
   );
 }
@@ -40,24 +66,22 @@ function Question({ label, type, setValue, readOnly }) {
  *  onSubmit: the callback that receives a response from the widget(s)
  */
 function ContextualQuestions({ onSubmit }) {
+  let [atHomeOrWork, setAtHomeOrWork] = useState(undefined);
   let [location, setLocation] = useState(undefined);
-  let [time, setTime] = useState(undefined);
   let [whoWith, setWhoWith] = useState(undefined);
-  let [atHome, setAtHome] = useState(undefined);
-  let [readOnly, setReadOnly] = useState(false);
+  let [inputDisabled, setInputDisabled] = useState(false);
 
-  let buttonClickable = location || time || whoWith || atHome; // if any of the fields are filled, the button is clickable
+  let buttonClickable = location || whoWith || atHomeOrWork; // if any of the fields are filled, the button is clickable
 
   // add the inputs to the onSubmit callback and set input to readonly
   function saveResponses() {
     onSubmit({
       type: "contextualQuestions",
       location: location,
-      timeOfDay: time,
       whoWith: whoWith,
-      atHome: atHome,
+      atHome: atHomeOrWork,
     });
-    setReadOnly(true);
+    setInputDisabled(true);
   }
 
   return (
@@ -74,38 +98,32 @@ function ContextualQuestions({ onSubmit }) {
           <IonGrid>
             <IonRow>
               <IonCol>
-                <Question
-                  label="Where are you?"
-                  type="text"
-                  placeholder="home"
-                  setValue={setLocation}
-                  readOnly={readOnly}
-                />
+                <QuestionChoice
+                  label={"Are you at home, or work?"}
+                  choices={["Home", "Work"]}
+                  value={atHomeOrWork}
+                  setValue={setAtHomeOrWork}
+                  disabled={inputDisabled}
+                ></QuestionChoice>
               </IonCol>
               <IonCol>
-                <Question
-                  label="What time is it?"
-                  type="text"
-                  setValue={setTime}
-                  readOnly={readOnly}
+                <QuestionChoice
+                  label="Are you moving alone, or with others?"
+                  choices={["Alone", "With others"]}
+                  value={whoWith}
+                  setValue={setWhoWith}
+                  disabled={inputDisabled}
                 />
               </IonCol>
             </IonRow>
             <IonRow>
               <IonCol>
-                <Question
-                  label="Are you at home, or work?"
+                <QuestionText
+                  label="Where are you, i.e. office or living room?"
                   type="text"
-                  setValue={setAtHome}
-                  readOnly={readOnly}
-                />
-              </IonCol>
-              <IonCol>
-                <Question
-                  label="Are you moving alone?"
-                  type="text"
-                  setValue={setWhoWith}
-                  readOnly={readOnly}
+                  placeholder="home"
+                  setValue={setLocation}
+                  disabled={inputDisabled}
                 />
               </IonCol>
             </IonRow>
@@ -115,7 +133,7 @@ function ContextualQuestions({ onSubmit }) {
           <IonButton
             expand="block"
             onClick={saveResponses}
-            disabled={!buttonClickable || readOnly} // button only clickable if a field is filled, or if answers not submitted
+            disabled={!buttonClickable || inputDisabled} // button only clickable if a field is filled, or if answers not submitted
           >
             Save Responses <IonIcon icon={arrowForwardOutline} />
           </IonButton>
