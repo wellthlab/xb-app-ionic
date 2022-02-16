86e3a71fe8f1b5de44fbae306dcca1f5340849c0
Edward Parkinson <e.parkinson@soton.ac.uk>
2022-02-16 12:13:54 +0000

    change layout and make easier to see when inputs submitted

--- xbapp/src/Strength/Questions.jsx
+++ xbapp/src/Strength/Questions.jsx
@@ -16,7 +16,7 @@ import {
 } from "@ionic/react";
 import { arrowForwardOutline } from "ionicons/icons";
 
-function Question({ label, type, setValue }) {
+function Question({ label, type, setValue, readOnly }) {
   return (
     <>
       <IonCol>
@@ -27,6 +27,7 @@ function Question({ label, type, setValue }) {
             onIonChange={(e) => {
               setValue(e.detail.value);
             }}
+            readonly={readOnly}
           ></IonInput>
         </IonItem>
       </IonCol>
@@ -35,22 +36,28 @@ function Question({ label, type, setValue }) {
 }
 
 /**
- *  Contextual questions about where the user is exercising, etc.
+ *  Contextual questions about where the user is exercising, etc. Set in a 2-column grid.
  *  onSubmit: the callback that receives a response from the widget(s)
  */
 function ContextualQuestions({ onSubmit }) {
-  let [location, setLocation] = useState("");
-  let [time, setTime] = useState("");
-  let [whoWith, setWhoWith] = useState("");
-  let [atHome, setAtHome] = useState("");
+  let [location, setLocation] = useState(undefined);
+  let [time, setTime] = useState(undefined);
+  let [whoWith, setWhoWith] = useState(undefined);
+  let [atHome, setAtHome] = useState(undefined);
+  let [readOnly, setReadOnly] = useState(false);
 
+  let buttonClickable = location || time || whoWith || atHome; // if any of the fields are filled, the button is clickable
+
+  // add the inputs to the onSubmit callback and set input to readonly
   function saveResponses() {
     onSubmit({
+      type: "contextualQuestions",
       location: location,
       timeOfDay: time,
       whoWith: whoWith,
       atHome: atHome,
     });
+    setReadOnly(true);
   }
 
   return (
@@ -66,38 +73,50 @@ function ContextualQuestions({ onSubmit }) {
         <IonCardContent>
           <IonGrid>
             <IonRow>
-              <Question
-                label="Where are you?"
-                type="text"
-                placeholder="home"
-                setValue={setLocation}
-              />
+              <IonCol>
+                <Question
+                  label="Where are you?"
+                  type="text"
+                  placeholder="home"
+                  setValue={setLocation}
+                  readOnly={readOnly}
+                />
+              </IonCol>
+              <IonCol>
+                <Question
+                  label="What time is it?"
+                  type="text"
+                  setValue={setTime}
+                  readOnly={readOnly}
+                />
+              </IonCol>
             </IonRow>
             <IonRow>
-              <Question
-                label="What time is it?"
-                type="text"
-                setValue={setTime}
-              />
-            </IonRow>
-            <IonRow>
-              <Question
-                label="Are you at home, or work?"
-                type="text"
-                setValue={setAtHome}
-              />
-            </IonRow>
-            <IonRow>
-              <Question
-                label="Are you moving alone?"
-                type="text"
-                setValue={setWhoWith}
-              />
+              <IonCol>
+                <Question
+                  label="Are you at home, or work?"
+                  type="text"
+                  setValue={setAtHome}
+                  readOnly={readOnly}
+                />
+              </IonCol>
+              <IonCol>
+                <Question
+                  label="Are you moving alone?"
+                  type="text"
+                  setValue={setWhoWith}
+                  readOnly={readOnly}
+                />
+              </IonCol>
             </IonRow>
           </IonGrid>
         </IonCardContent>
         <div className="row" style={{ textAlign: "center" }}>
-          <IonButton onClick={saveResponses}>
+          <IonButton
+            expand="block"
+            onClick={saveResponses}
+            disabled={!buttonClickable || readOnly} // button only clickable if a field is filled, or if answers not submitted
+          >
             Save Responses <IonIcon icon={arrowForwardOutline} />
           </IonButton>
         </div>
