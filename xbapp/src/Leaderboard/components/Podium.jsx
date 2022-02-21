f05c433f2fdb02f465ff2cde073eb8a38f4689db
Brian Le <brianle1301@gmail.com>
2022-02-21 04:19:44 +0000

    Fix indentation

--- xbapp/src/Leaderboard/components/Podium.jsx
+++ xbapp/src/Leaderboard/components/Podium.jsx
@@ -7,59 +7,59 @@ import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 
 const Spotlight = function ({ overall, name }) {
 
-    return (
-        <div className="spotlight">
-            <CircularProgressbarWithChildren
-                value={overall.percentage * 100}
-                styles={{
-                    path: {
-                        stroke: 'var(--ion-color-success)',
-                        width: 8,
-                        strokeLinecap: 'butt',
-                    },
-                    trail: {
-                        stroke: 'var(--ion-color-light)',
-                    },
-                }}
-            >
-                <span className="score">{Math.round(overall.percentage * 100)}%</span>
-                {name}
-            </CircularProgressbarWithChildren>
-        </div>
-    );
+  return (
+    <div className="spotlight">
+      <CircularProgressbarWithChildren
+        value={overall.percentage * 100}
+        styles={{
+          path: {
+            stroke: 'var(--ion-color-success)',
+            width: 8,
+            strokeLinecap: 'butt',
+          },
+          trail: {
+            stroke: 'var(--ion-color-light)',
+          },
+        }}
+      >
+        <span className="score">{Math.round(overall.percentage * 100)}%</span>
+        {name}
+      </CircularProgressbarWithChildren>
+    </div>
+  );
 };
 
 const Podium = function ({ teams }) {
 
-    return (
-        <div className="podium-wrapper">
-            {teams[1] && (
-                <div className="secondary-spotlight">
-                    <div className="stars">
-                        <IonIcon icon={star} />
-                        <IonIcon icon={star} />
-                    </div>
-                    <Spotlight {...teams[1]} />
-                </div>
-            )}
-            <div className="primary-spotlight">
-                <div className="stars">
-                    <IonIcon icon={star} />
-                    <IonIcon icon={star} />
-                    <IonIcon icon={star} />
-                </div>
-                <Spotlight {...teams[0]} />
-            </div>
-            {teams[2] && (
-                <div className="secondary-spotlight">
-                    <div className="stars">
-                        <IonIcon icon={star} />
-                    </div>
-                    <Spotlight {...teams[2]} />
-                </div>
-            )}
+  return (
+    <div className="podium-wrapper">
+      {teams[1] && (
+        <div className="secondary-spotlight">
+          <div className="stars">
+            <IonIcon icon={star} />
+            <IonIcon icon={star} />
+          </div>
+          <Spotlight {...teams[1]} />
         </div>
-    );
+      )}
+      <div className="primary-spotlight">
+        <div className="stars">
+          <IonIcon icon={star} />
+          <IonIcon icon={star} />
+          <IonIcon icon={star} />
+        </div>
+        <Spotlight {...teams[0]} />
+      </div>
+      {teams[2] && (
+        <div className="secondary-spotlight">
+          <div className="stars">
+            <IonIcon icon={star} />
+          </div>
+          <Spotlight {...teams[2]} />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Podium;
--- xbapp/src/Leaderboard/components/TeamCard.jsx
+++ xbapp/src/Leaderboard/components/TeamCard.jsx
@@ -3,19 +3,19 @@ import { IonCard, IonItem, IonProgressBar, IonLabel } from '@ionic/react';
 
 const TeamCard = function ({ team, order }) {
 
-    return (
-        <IonCard>
-            <IonItem>
-                <IonLabel>{order}. {team.name}</IonLabel>
-                <IonLabel>{team.userCount} member(s)</IonLabel>
-                <IonLabel>{Math.round(team.overall.minutes)} minutes exercised</IonLabel>
-                <IonLabel>
-                    {Math.round(team.overall.percentage * 100)}%
+  return (
+    <IonCard>
+      <IonItem>
+        <IonLabel>{order}. {team.name}</IonLabel>
+        <IonLabel>{team.userCount} member(s)</IonLabel>
+        <IonLabel>{Math.round(team.overall.minutes)} minutes exercised</IonLabel>
+        <IonLabel>
+          {Math.round(team.overall.percentage * 100)}%
                     <IonProgressBar color="success" value={team.overall.percentage} />
-                </IonLabel>
-            </IonItem>
-        </IonCard>
-    );
+        </IonLabel>
+      </IonItem>
+    </IonCard>
+  );
 };
 
 export default TeamCard;
