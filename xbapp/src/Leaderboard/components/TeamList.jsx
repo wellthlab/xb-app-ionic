637b016543dd59aba34ae428b006ad8a0aae13e6
Brian Le <brianle1301@gmail.com>
2022-03-04 15:55:08 +0000

    Add daily breakdown

--- xbapp/src/Leaderboard/components/TeamList.jsx
+++ xbapp/src/Leaderboard/components/TeamList.jsx
@@ -1,41 +1,71 @@
-import './TeamList.css';
+import "./TeamList.css";
 
-import React from 'react';
-import { IonCard, IonLabel, IonItem } from '@ionic/react';
-import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import React from "react";
+import { IonCard, IonLabel, IonItem } from "@ionic/react";
+import { CircularProgressbarWithChildren } from "react-circular-progressbar";
 
-const strokeColors = ['#FFD700', '#C0C0C0', '#CD7F32'];
+const strokeColors = ["#FFD700", "#C0C0C0", "#CD7F32"];
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+const CircularProgress = function ({ value, color, content }) {
+  return (
+    <CircularProgressbarWithChildren
+      value={value * 100}
+      styles={{
+        path: {
+          stroke: color || "var(--ion-color-success)",
+          strokeLinecap: "butt",
+        },
+        trail: {
+          stroke: "var(--ion-color-light)",
+        },
+      }}
+    >
+      {content}
+    </CircularProgressbarWithChildren>
+  );
+};
 
 const TeamList = function ({ teams }) {
-
   return (
     <div className="team-list">
       {teams.map((team, i) => (
         <IonCard key={team._id} className="team-card">
           <IonItem>
             <div className="progress-wrapper">
-              <CircularProgressbarWithChildren
-                value={team.overall.completion * 100}
-                styles={{
-                  path: {
-                    stroke: strokeColors[i] || 'var(--ion-color-success)',
-                    strokeLinecap: 'butt',
-                  },
-                  trail: {
-                    stroke: 'var(--ion-color-light)',
-                  },
-                }}
-              >
-                {i + 1}
-              </CircularProgressbarWithChildren>
+              <CircularProgress
+                value={team.overall.completion}
+                color={strokeColors[i]}
+                content={i + 1}
+              />
             </div>
             <IonLabel>
               <h2>{team.name}</h2>
               <p>{team.userCount} member(s)</p>
             </IonLabel>
-            <IonLabel slot="end" className="progress ion-text-end">
+            {team.overall.dailyBreakdown && (
+              <div className="breakdown-wrapper">
+                {daysOfWeek.map((day, i) => (
+                  <div className="breakdown-progress" key={i}>
+                    {day}
+                    <CircularProgress
+                      value={team.overall.dailyBreakdown[i] || 0}
+                      content={
+                        team.overall.dailyBreakdown[i]
+                          ? Math.round(team.overall.dailyBreakdown[i] * 100)
+                          : "N/A"
+                      }
+                    />
+                  </div>
+                ))}
+              </div>
+            )}
+            <IonLabel className="progress ion-text-end">
               <h2>{Math.round(team.overall.completion * 100)}%</h2>
-              <p>{Math.round(team.overall.cappedMinutes)} mins / {Math.round(team.overall.target)} mins target</p>
+              <p>
+                {Math.round(team.overall.cappedMinutes)} mins /{" "}
+                {Math.round(team.overall.target)} mins target
+              </p>
             </IonLabel>
           </IonItem>
         </IonCard>
