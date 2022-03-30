1de29823ab4244dd219af92685318638bec8f202
Edward Parkinson <e.parkinson@soton.ac.uk>
2022-03-30 13:18:40 +0100

    add default movements to EDT and exercises to response

--- xbapp/src/Strength/EDTTask.jsx
+++ xbapp/src/Strength/EDTTask.jsx
@@ -13,14 +13,50 @@ import {
 import TimerEDT from "./components/EDTMovementTimer";
 import { getMove } from "../DEPRECATED/components/OLDMovementPicker";
 
+function getDefaultMove(type) {
+  switch (type) {
+    // Bilateral
+    case "bilateral lower push":
+      return "fullsquat";
+    case "bilateral lower pull":
+      return "wallrdlprep";
+    case "bilateral upper push":
+      return "flatpushup";
+    case "bilateral upper pull":
+      return "chinup";
+    // Unilateral
+    // case "unilateral lower push":
+    //   return "lunge";
+    // case "unilateral lower pull":
+    //   return "singlelegromaniandeadlift";
+    // case "unilateral upper push":
+    //   return "archerpushup";
+    // case "unilateral upper pull":
+    //   return "flatpushup";
+    // Isometric
+    case "isolateral lower push":
+      return "vsit";
+    case "isolateral lower pull":
+      return "lsit";
+    case "isolateral upper push":
+      return "sideplank";
+    case "isolateral upper pull":
+      return "onearmpull";
+    default:
+      return "fullsquat";
+  }
+}
+
+//
+
 /**
  *  EDT task - includes the ability to change moves
  *
  */
 function EDTSet({ task, onSubmit }) {
   // TODO: go back to movement picker when ready
-  let moveA = getMove(task.edtMoves[0]);
-  let moveB = getMove(task.edtMoves[1]);
+  let moveA = getMove(getDefaultMove(task.edtMoves[0]));
+  let moveB = getMove(getDefaultMove(task.edtMoves[1]));
 
   return (
     <>
