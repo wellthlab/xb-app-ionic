6c10417dd8a219b0a16153dfa9effa2593b94ca3
Brian Le <brianle1301@gmail.com>
2022-07-21 15:06:17 +0100

    Change intro

--- xbapp/src/move/ModulesPicker/ExperimentInfo.jsx
+++ xbapp/src/move/ModulesPicker/ExperimentInfo.jsx
@@ -2,11 +2,6 @@ import React from "react";
 
 import XBInfo from "../../util/XBInfo";
 
-const teamsLink =
-  "https://teams.microsoft.com/l/channel/19%3ab869ef6a42f14acd95036bd40" +
-  "5f131c7%40thread.tacv2/app%2520discussion?groupId=11c4aee6-b0d1-4221" +
-  "-adbe-1350280f6ef0&tenantId=4a5378f9-29f4-4d3e-be89-669d03ada9d8";
-
 const ExperimentInfo = function () {
   return (
     <XBInfo title="Welcome to XB!">
@@ -15,8 +10,7 @@ const ExperimentInfo = function () {
         <u>beta</u>
       </strong>{" "}
       app, we're glad you're here! Please report any bugs and share any
-      feedback/suggestions in the{" "}
-      <a href={teamsLink}>sinwork-discussion team</a>
+      feedback/suggestions in Microsoft Teams
     </XBInfo>
   );
 };
