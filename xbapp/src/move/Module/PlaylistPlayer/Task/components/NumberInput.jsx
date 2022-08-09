93b121fb68e575c7186278bd5919a2fdc43d994a
Brian Le <brianle1301@gmail.com>
2022-08-09 04:32:21 +0100

    Clean up

--- xbapp/src/move/Module/PlaylistPlayer/Task/components/NumberInput.jsx
+++ xbapp/src/move/Module/PlaylistPlayer/Task/components/NumberInput.jsx
@@ -5,12 +5,6 @@ import TextInput from "./TextInput";
 const NumberInput = function ({ value, onIonChange, ...others }) {
   const handleChange = function (newValue) {
     if (onIonChange) {
-      console.log(newValue, value);
-      if (!newValue) {
-        onIonChange(null);
-        return;
-      }
-
       if (/^\d*(\.\d*)?$/.test(newValue)) {
         onIonChange(newValue);
         return;
