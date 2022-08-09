aa40ff3f14faf6a39232582e54515762f5660be4
Brian Le <brianle1301@gmail.com>
2022-08-09 04:19:06 +0100

    Cleanup

--- xbapp/src/move/Module/ModuleContents.jsx
+++ xbapp/src/move/Module/ModuleContents.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRouteMatch, useParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import {
   IonPage,
   IonContent,
