fb325ca45016ee6ec0c6d69e3442c9624d0a09b2
Brian Le <brianle1301@outlook.com>
2023-05-01 18:34:29 +0100

    Enhance onboarding experience

--- src/screens/onboarding/StudyInformation.tsx
+++ src/screens/onboarding/StudyInformation.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { Button } from '@mui/joy';
+import { Box, Button } from '@mui/joy';
 
 import Page from '../../components/foundation/Page';
 
 import AboutThisStudy from '../../components/AboutThisStudy';
 
 const StudyInformation = function () {
+    const [canContinue, setCanContinue] = React.useState(false);
+
+    const handleScroll = function (e: React.UIEvent<HTMLElement>) {
+        const target = e.currentTarget;
+
+        if (Math.abs(target.scrollHeight - target.clientHeight - target.scrollTop) < 1) {
+            setCanContinue(true);
+        }
+    };
+
     return (
-        <Page>
-            <AboutThisStudy />
-            <Button component={RouterLink} to="/onboarding/consent" sx={{ mt: 4 }} fullWidth>
-                Next
+        <Page sx={{ height: '100vh' }}>
+            <Box sx={{ flex: 1, overflow: 'auto' }} onScroll={handleScroll}>
+                <AboutThisStudy />
+            </Box>
+
+            <Button disabled={!canContinue} component={RouterLink} to="/onboarding/consent" sx={{ mt: 2 }} fullWidth>
+                {canContinue ? 'Next' : 'Scroll or read to the end to continue'}
             </Button>
         </Page>
     );
