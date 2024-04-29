9631136c25afacd3c4d4580c1216875388d58d87
Jamie Court <jc2a23@soton.ac.uk>
2024-04-29 16:36:43 +0100

    Adding string dict

---
+++ src/utils/string_dict.js
@@ -0,0 +1,14 @@
+const Strings = {
+	email_is_missing : "Email is missing",
+	please_input_an_email_address : "Please input an email address",
+	password_is_missing : "Password is missing",
+	password_confirmation_is : "Password confirmation is missing",
+	passwords_must_match : "Passwords must match",
+	sorry_cannot_submit_this_form : "Sorry, cannot submit this form at the moment",
+	user_denied_permissions : "User denied permissions!",
+	sorry_the_invite_code_did_not : "Sorry, the invite code did not correspond to any team",
+	this_method_should_only_be : "This method should only be called when user is authenticated",
+
+};
+
+export default Strings;
