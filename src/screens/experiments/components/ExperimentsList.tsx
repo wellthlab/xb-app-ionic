e8471f9e0d861f1f3884a7ffdf937962a8554201
coo1f24 <c.o.okorie@soton.ac.uk>
2024-06-03 09:58:05 +0100

    cleaning up code

--- src/screens/experiments/components/ExperimentsList.tsx
+++ src/screens/experiments/components/ExperimentsList.tsx
@@ -21,7 +21,7 @@ interface IExperimentsListProps {
     isSubscribedToBox?: boolean
 }
 
-const ExperimentsList = function({experiments, onExperimentSelected, isCheckBoxSelected, isSubscribedToBox }: IExperimentsListProps) {
+const ExperimentsList = function({ experiments, onExperimentSelected, isCheckBoxSelected, isSubscribedToBox }: IExperimentsListProps) {
     const experiments_  = JSON.parse(JSON.stringify(experiments)) as GenericExperiment[];
     let boxWeeks = experiments_.map((e: { boxWeek: number; }) => {
         if (!e.boxWeek) {
