a83f16d8c8b8597997a272b5fead6c0e3306fd2d
Brian Le <brianle1301@outlook.com>
2024-06-21 14:52:13 +0100

    Revert "Merge pull request #167 from wellthlab/revert-161-welcome-lean"
    This reverts commit 2539bb63aa82803b2e7b9647ef6d3e88e924ff03, reversing
    changes made to a9f744c696e5dae2342cfbe5ee5da8a07d7ecaba.

--- src/screens/onboarding/Consent.tsx
+++ src/screens/onboarding/Consent.tsx
@@ -1,4 +1,4 @@
-import Strings from '../../utils/string_dict.js';
+import Strings from '../../utils/string_dict';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import * as Yup from 'yup';
@@ -8,22 +8,44 @@ import PageTitle from '../../components/foundation/PageTitle';
 import Form from '../../components/foundation/Form';
 import Checkbox from '../../components/foundation/Checkbox';
 import useForm from '../../components/foundation/useForm';
-import ExerciseWarning from '../../components/ExerciseWarning';
+
+import useStudy from '../../hooks/useStudy';
 
 const checkboxSchema = Yup.bool().oneOf([true], Strings.please_check_this_box_to);
 
-const schema = Yup.object().shape({
-    c1: checkboxSchema,
-    c2: checkboxSchema,
-    c3: checkboxSchema,
-    c4: checkboxSchema,
-});
-
 const Consent = function () {
-    const { getCheckboxProps, createHandleSubmit, form } = useForm(
-        { c1: false, c2: false, c3: false, c4: false },
-        schema,
+    const { isPending } = useStudy();
+
+    return (
+        <Page>
+            <PageTitle>{Strings.just_a_few_things}</PageTitle>
+            {isPending ? 'Loading...' : <ConsentForm />}
+        </Page>
     );
+};
+
+const ConsentForm = function () {
+    const { study } = useStudy();
+
+    const initialFormState = React.useMemo(
+        () =>
+            study!.consent.reduce((acc, v, i) => {
+                acc[`c${i}`] = false;
+                return acc;
+            }, {} as Record<string, boolean>),
+        [study],
+    );
+
+    const initialSchema = React.useMemo(() => {
+        const schemaKeys = study!.consent.reduce((acc, v, i) => {
+            acc[`c${i}`] = checkboxSchema;
+            return acc;
+        }, {} as Record<string, Yup.AnySchema>);
+
+        return Yup.object().shape(schemaKeys);
+    }, [study]);
+
+    const { getCheckboxProps, createHandleSubmit, form } = useForm(initialFormState, initialSchema);
 
     const history = useHistory();
     const handleSubmit = createHandleSubmit(() => {
@@ -31,31 +53,11 @@ const Consent = function () {
     });
 
     return (
-        <Page>
-            <PageTitle>{Strings.just_a_few_things}</PageTitle>
-
-            <Form submitButtonColor="success" submitLabel={Strings.enroll} message={form.errors.$root} onSubmit={handleSubmit}>
-                <Checkbox
-                    label={Strings.i_have_read_the_provided}
-                    {...getCheckboxProps('c1')}
-                />
-                <Checkbox
-                    label={Strings.i_am_a_member_of_staff_or_a}
-                    {...getCheckboxProps('c2')}
-                />
-                <Checkbox
-                    label={Strings.i_understand_that_i_must_be}
-                    {...getCheckboxProps('c3')}
-                />
-
-                <ExerciseWarning />
-
-                <Checkbox
-                    label={Strings.i_understand_that_physical}
-                    {...getCheckboxProps('c4')}
-                />
-            </Form>
-        </Page>
+        <Form submitLabel={Strings.next} message={form.errors.$root} onSubmit={handleSubmit}>
+            {study!.consent.map((statement, i) => (
+                <Checkbox key={i} label={statement} {...getCheckboxProps(`c${i}`)} />
+            ))}
+        </Form>
     );
 };
 
--- src/screens/onboarding/NewProfile.tsx
+++ src/screens/onboarding/NewProfile.tsx
@@ -1,16 +1,28 @@
-import Strings from '../../utils/string_dict.js';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Page from '../../components/foundation/Page';
 import PageTitle from '../../components/foundation/PageTitle';
 
-import ProfileForm from '../../components/ProfileForm';
+import ProfileForm, { IProfileFormProps } from '../../components/ProfileForm';
+import { useDispatch } from '../../slices/store';
+import { setProfile } from '../../slices/onboarding';
+
+import Strings from '../../utils/string_dict';
 
 const NewProfile = function () {
+    const dispatch = useDispatch();
+
+    const history = useHistory();
+    const handleSubmit: IProfileFormProps['onSubmit'] = function (data) {
+        dispatch(setProfile(data));
+        history.push('/onboarding/welcome/0');
+    };
+
     return (
         <Page>
             <PageTitle>{Strings.your_profile}</PageTitle>
-            <ProfileForm />
+            <ProfileForm onSubmit={handleSubmit} />
         </Page>
     );
 };
