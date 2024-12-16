74c5051f857d7a4790d0cb878e2cbf53abb0ee9d
coo1f24 <c.o.okorie@soton.ac.uk>
2024-12-16 13:05:59 +0000

    Add about tab

--- src/screens/onboarding/Consent.tsx
+++ src/screens/onboarding/Consent.tsx
@@ -1,6 +1,6 @@
 import Strings from '../../utils/string_dict';
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import * as Yup from 'yup';
 
 import Page from '../../components/foundation/Page';
@@ -13,11 +13,10 @@ import useStudy from '../../hooks/useStudy';
 
 const checkboxSchema = Yup.bool().oneOf([true], Strings.please_check_this_box_to);
 
-export interface IConsentFormProps {
-    isAboutPage: boolean
-}
-const ConsentForm = function ( {isAboutPage} :  IConsentFormProps) {
+const ConsentForm = function () {
     const { study } = useStudy();
+    const location = useLocation();
+    const isAboutPage = location.pathname.includes('about');
 
    const initialFormState = React.useMemo(
         () =>
@@ -25,7 +24,7 @@ const ConsentForm = function ( {isAboutPage} :  IConsentFormProps) {
                 acc[`c${i}`] = isAboutPage;
                 return acc;
             }, {} as Record<string, boolean>),
-        [study],
+        [study, isAboutPage],
     );
 
     const initialSchema = React.useMemo(() => {
